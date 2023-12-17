0b46874e54efb185289770e6cd2cbcdf82de22bd
Vikram <vikik@LAPTOP-5520CVSU>
2023-12-17 18:52:32 -0500

    Add reporting to merge across different splits

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from 'cypress'
 import cypressSplit from 'cypress-split'
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    useInlineDiffs: true,
+    embeddedScreenshots: true,
+    reportDir: 'cypress/results',
+    reportFilename: '[name].html',
+    overwrite: true,
+    html: true,
+    json: true,
+  },
   e2e: {
     // baseUrl, etc
-    supportFile: false,
+    // supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/spec-*.cy.js',
     fixturesFolder: false,
     setupNodeEvents(on, config) {
       cypressSplit(on, config)
